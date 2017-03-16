c66d6633370f2749853537466b1420ff9b48f786
Jacobo Sacal <jacobosacal@Jacobos-MacBook-Pro.local>
2017-03-15 20:34:17 -0600

    new pods and picker view

--- Trades/AddViewController.swift
+++ Trades/AddViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 import Firebase
 
 class AddViewController: UIViewController {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var listaDeCategorias = ["Arte", "Calzado", "Cocina", "Deportes", "Ejercicio", "Hogar", "Ofinia", "Ropa", "Tecnologia", "Otros",]
 
     @IBOutlet weak var name: UITextField!
     
@@ -18,7 +22,10 @@ class AddViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         addGestureToView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -29,4 +36,24 @@ class AddViewController: UIViewController {
         
     }
 
+}
+
+extension AddViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return listaDeCategorias[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return listaDeCategorias.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(listaDeCategorias[pickerView.selectedRow(inComponent: 0)])
+        var categoriaSeleccionada = listaDeCategorias[pickerView.selectedRow(inComponent: 0)]
+    }
 }
