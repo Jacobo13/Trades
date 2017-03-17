413de029f4d370c1cd853afca1c715c160b190de
Jacobo Sacal <jacobosacal@Jacobos-MacBook-Pro.local>
2017-03-16 19:49:47 -0600

    hola

---
+++ Pods/Target Support Files/Pods-Trades/Pods-Trades-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TradesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TradesVersionString[];
+
---
+++ Pods/Target Support Files/Pods-TradesTests/Pods-TradesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TradesTests : NSObject
+@end
+@implementation PodsDummy_Pods_TradesTests
+@end
