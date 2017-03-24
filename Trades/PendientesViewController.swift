903c0b91d8204297acc36a74bad614a877496767
DanielDarwich <ddarwichh@atid.edu.mx>
2017-03-24 16:23:36 -0600

    gorda

---
+++ Trades/PendientesViewController.swift
@@ -0,0 +1,60 @@
+//
+//  PendientesViewController.swift
+//  Trades
+//
+//  Created by Programación Daniel on 24/03/17.
+//  Copyright © 2017 Jacobo Sacal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class PendientesViewController : UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
